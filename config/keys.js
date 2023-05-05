a5348e63328ac36a1e05cf3d7d0b447f9e6399ce
Shivanshudeveloper <developer.shivanshu@gmail.com>
2023-05-05 09:59:28 +0530

    Backend

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 module.exports = {
-  MongoURI:
-    "mongodb+srv://admin:Roastchicken123!!@cluster0.tfeoazh.mongodb.net/?retryWrites=true&w=majority",
+  // MongoURI:
+  //   "mongodb+srv://mvc:mvc@cluster0.6myeufy.mongodb.net/?retryWrites=true&w=majority",
+
+  MongoURI: "mongodb+srv://doadmin:3f46v0L17Au5U2Ha@db-mongodb-nyc1-91794-cd6a12fd.mongo.ondigitalocean.com/admin",
+
   PUBLIC_URL:
     'http://localhost:3000'
   // PUBLIC_URL:
